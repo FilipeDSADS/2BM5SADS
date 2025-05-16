59008b6024d21597c35f33d41f90574478741772
FilipeDSADS <filipeaugustodsds@gmail.com>
2025-05-15 22:16:59 -0300

    Add files via upload

---
+++ supremo/src/main/java/com/bn/supremo/SupremoApplication.java
@@ -0,0 +1,13 @@
+package com.bn.supremo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupremoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupremoApplication.class, args);
+	}
+
+}
---
+++ supremo/src/main/java/com/bn/supremo/pessoa/UserController.java
@@ -0,0 +1,5 @@
+package com.bn.supremo.pessoa;
+
+public class UserController {
+
+}
